b40ef7e8434b406de851592c9e9fffad0b14643a
datguyducky <36929645+datguyducky@users.noreply.github.com>
2026-03-25 21:14:26 +0100

    chore: update extension links and add redirects for Chrome and Firefox

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	redirects: {
+		'/install/chrome':
+			'https://chromewebstore.google.com/detail/inboxt/ipbkogoedgllpgmfbhciclanlncfoemp',
+		'/install/firefox': 'https://addons.mozilla.org/en-US/firefox/addon/inboxt/',
+	},
 });
